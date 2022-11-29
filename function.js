2a2539260e7ded6041505e78606fe94e377692af
Gedeon4ick <pavell720@gmaul.com>
2022-11-29 18:39:57 +0600

    funcrion argument

--- function.js
+++ function.js
@@ -15,3 +15,17 @@ function calc(a, b) {
 console.log(calc(3, 5));
 console.log(calc(2, 5));
 console.log(calc(1, 5));
+
+// Конвертор денег
+
+const  usdCurr = 28;
+const  eurCurr = 32;
+
+function convert(amount, curr) {
+    console.log(curr * amount);
+}
+
+convert(500, usdCurr);
+convert(500, eurCurr);
+
+
