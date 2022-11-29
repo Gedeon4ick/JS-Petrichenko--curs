62ca206a1b1e46d593a1956d4ff148c45895b967
Gedeon4ick <pavell720@gmaul.com>
2022-11-29 20:23:41 +0600

    funcrion  3 exercise

--- function.js
+++ function.js
@@ -59,4 +59,27 @@ function returnNeighboringNumbers(i = 6) {
 console.log(returnNeighboringNumbers());
 
 
+// 3 exercise 
 
+function getMathResult(num, times) {
+    if (typeof(times) !== 'number' || times <= 0) {
+        return num;
+    }
+
+    let str = '';
+
+    for (let i = 1; i <= times; i++) {
+        if (i === times) {
+            str += `${num * i}`;
+            // Тут без черточек в конце
+        } else {
+            str += `${num * i}---`;
+            // Это тоже самое, что и
+            // str = str + num * i + "---"
+        }
+    }
+
+    return str;
+}
+
+getMathResult(10, 5);
