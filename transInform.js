a1536ec7bf54ea5ee230b9ab4ac21ba75cf885f3
Gedeon4ick <pavell720@gmaul.com>
2022-12-02 11:39:39 +0600

    method copy array

---
+++ transInform.js
@@ -0,0 +1,76 @@
+"use strict";
+
+// Передача информации по ссылке или по значению пишемфункцию для копирования обьекта
+// 1 cспособ
+
+function copy(mainObj) {
+    let objCopy = {};
+
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+
+    return objCopy;  
+}
+
+//ТЕСТ
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+ 
+const newNumbers = copy(numbers);
+
+newNumbers.a = 10;
+console.log(newNumbers);
+console.log(numbers);
+
+// 2способ. Соединение (копирование) обьектов с помощью Object.assign()
+
+const add = {
+    d: 17,
+    e:20
+};
+
+// console.log(Object.assign(numbers, add));
+
+// копирование 
+
+const clone = Object.assign({}, add);
+console.log(clone);
+
+// метод создания копий массива
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = [...oldArray];
+
+console.log(newArray);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+// у нас есть массив который необхоимо передать функции
+
+const num = [2, 5, 7];
+
+// Вызываем функцию методом splet таким образом передаем наш массив в аргумент функции
+
+log(...num);
+
+// копирование обьекта с помощью spret оператора
+
+const obj = {
+    first: 1,
+    second: 2
+};
+
+const newObject = {...obj};
+console.log(newObject);
