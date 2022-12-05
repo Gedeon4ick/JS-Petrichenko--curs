84e6b4b0f59981c7d8d5fa3d1dc38b0b45a7a85a
Gedeon4ick <pavell720@gmaul.com>
2022-12-05 20:13:46 +0600

    oldmetods

--- elements/js/script.js
+++ elements/js/script.js
@@ -5,7 +5,7 @@ const btns = document.getElementsByTagName('button');
 const circle = document.getElementsByClassName('circle');
 const hearts = document.querySelectorAll('.heart');
 const oneHearts = document.querySelector('.heart');
-
+const wrapper = document.querySelector('.wrapper');
 // box.style.backgroundColor = 'blue';
 // box.style.width = '500px';
 
@@ -21,3 +21,30 @@ btns[1].style.borderRadius = '100%';
 hearts.forEach(item => {
     item.style.backgroundColor = 'blue';
 });
+
+const div = document.createElement('div');
+// const text = document.createTextNode('Тут был я')ж
+
+div.classList.add('black');   
+
+// document.body.append(div);
+wrapper.append(div);
+// wrapper.appendChild(div);
+// wrapper.prepend(div);
+
+// Вставка элемента перед другим элементом
+// hearts[0].before(div);
+// hearts[0].after(div);
+
+// Удаление элемента со страницы
+// circle[0].remove();
+
+// Замена одного элемента другим
+
+// hearts[0].replaceWith(circle[0]);
+
+// добавление HTML структуры настраницу
+// div.innerHTML = 'Hello World';
+
+// добавление текста в наш блок
+div.textContent = "Hello";
