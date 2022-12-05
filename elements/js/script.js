c1c87a96c5de34fae877b3d5edd4f05b6dc050ce
Gedeon4ick <pavell720@gmaul.com>
2022-12-05 16:19:14 +0600

    querySelectorAll

--- elements/js/script.js
+++ elements/js/script.js
@@ -1,6 +1,40 @@
 'use strict';
-
+// Обращаемся по индексу к элементам на странице
 // const box = document.getElementById('box');
-
 // console.log(box);
+
+// Обращаемся по тэгам к элементам на странице
+// const btns = document.getElementsByTagName('button');
+// console.log(btns);
+
+// Обращаемся к отдельной кнопке на странице по индивидуальному индексу
+
+// 1 вариант
+// const btns = document.getElementsByTagName('button')[1];
+// console.log(btns);
+
+// 2 вариант
+// console.log(btns[0]);
+
+// Метод обращения к классам
+
+// const circle = document.getElementsByClassName('circle');
+// console.log(circle);   
+
+// Современный querySelector имеет метод forEach
+
+// const hearts = document.querySelectorAll('.heart');
+// console.log(hearts);
+
+// hearts.forEach(item => {
+//     console.log(hearts);
+// });
+
+// Обращение к первому элементу массива
+
+const oneHearts = document.querySelector('.heart');
+console.log(oneHearts);
+
+
+
 
