7ce534d6e9d7bbb7d5e8803d0ee0f9f4d30bbcfb
Gedeon4ick <pavell720@gmaul.com>
2022-12-07 16:45:27 +0600

    recursia

---
+++ navigationDOM.js
@@ -0,0 +1,15 @@
+//  console.log(document.head);
+//  console.log(document.documentElement);
+// Обращение ко всему документу HTML
+//  console.log(document.body.childNodes);
+
+// Получение первго потмка 
+//  console.log(document.body.firstChild);
+
+// Получение последнего потомка
+//  console.log(document.head);
+
+console.log(document.querySelector('#current').parentNode);
+
+
+ 
