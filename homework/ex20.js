8e58932c1959ff84632468f33f02431643422638
Gedeon4ick <pavell720@gmaul.com>
2022-12-18 12:50:56 +0600

    ex20

---
+++ homework/ex20.js
@@ -0,0 +1,33 @@
+const films = [{
+        name: 'Titanic',
+        rating: 9
+    },
+    {
+        name: 'Die hard 5',
+        rating: 5
+    },
+    {
+        name: 'Matrix',
+        rating: 8
+    },
+    {
+        name: 'Some bad film',
+        rating: 4
+    }
+];
+
+function showGoodFilms(arr) {
+    const res = []
+    arr.forEach(element => {
+        const newOb = Object.entries(element)
+        .map(item => item[1])
+        // .filter(item => item[1] >= 8)
+        // .map(item => item[0])
+        res.push(newOb)
+    });
+    const result = res.filter(item => item[1] >= 8)
+    .map(item => item[0]);
+    console.log(result);
+}
+
+showGoodFilms(films)
